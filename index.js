0773805b5bbec8090176c60a7af12f0d67daef62
evan <evandromariano49@gmail.com>
2024-02-21 09:04:17 -0300

    feat: criado autenticação de usuários

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/users.js";
+import videoRoutes from "./routes/videos.js";
+import commentRoutes from "./routes/comments.js";
+import auth from "./routes/auth.js";
+import cookieParser from "cookie-parser";
+
+// configs
 
 const app = express();
+app.use(cookieParser());
+app.use(express.json());
 
 dotenv.config()
 
@@ -13,6 +22,25 @@ const connect = () => {
     throw err;
   });
 }
+
+// routes
+
+app.use("/api/users", userRoutes);
+app.use("/api/videos", videoRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/auth", auth);
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Erro interno!";
+  return res.status(status).json({
+    success: false,
+    status,
+    message
+  })
+});
+
+// routes
 
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
